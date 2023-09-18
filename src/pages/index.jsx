43a2aca76036e51de9419080e2618b7938b81f71
ACF <alricfernandes11@gmail.com>
2023-09-18 13:51:36 -0400

    Styling and formatting health care provider and patient buttons with links

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,23 +2,26 @@ import Image from 'next/image';
 import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
-// import NavBar from "../components/NavBar.jsx";
 import Link from 'next/link';
 
 export default function Home() {
-
   return (
-    <>
+    <div className='flex flex-col justify-center items-center gap-20 bg-yellow-500 h-screen'>
       <Link href='/healthCareProviderHomePage'>
-        <button id='healthCareProviderButton' className='bg-green-500 border-4 border-violet-white gap-5'>
+        <button
+          type='button'
+          id='healthCareProviderButton'
+          className='rounded-t-2xl rounded-b-2xl p-3 border-radius-5 bg-green-500 border-4 border-black text-black text-5xl'
+        >
           HEALTH CARE PROVIDER
         </button>
       </Link>
       <Link href='/patientHomePage'>
-        <button id='patientButton' className='bg-green-500 border-4 border-violet-white gap-5'>
+        <button type='button' id='healthCareProviderButton' className='rounded-t-2xl rounded-b-2xl p-3 border-radius-5 bg-green-500 border-4 border-black text-black text-5xl'
+        >
           PATIENT
         </button>
       </Link>
-    </>
+    </div>
   );
 }
