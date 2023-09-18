46a6fdd3ac834ebfa14ee68e3545c4f59de2eff7
ACF <alricfernandes11@gmail.com>
2023-09-18 13:56:25 -0400

    Styling and formatting page

--- src/pages/healthCareProviderHomePage.jsx
+++ src/pages/healthCareProviderHomePage.jsx
@@ -2,8 +2,8 @@ import NavBar from '../components/NavBar';
 
 export default function healthCareProviderHomePage() {
   return (
-    <>
+    <div className='flex flex-col  justify-center bg-yellow-500 h-screen'>
       <NavBar />
-    </>
+    </div>
   );
 }
