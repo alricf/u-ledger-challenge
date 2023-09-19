5e77472121d9b1bafc5908968d8bc649419f1cca
ACF <alricfernandes11@gmail.com>
2023-09-19 14:05:08 -0400

    Adding medical record display functionality

--- src/pages/read.jsx
+++ src/pages/read.jsx
@@ -7,6 +7,7 @@ export default function read() {
 
   // Hooks
   const [transactionId, setTransactionId] = useState('');
+  const [payloadData, setPayloadData] = useState({});
 
   // Helper Functions
   const onChange = (e) => {
@@ -20,10 +21,13 @@ export default function read() {
     // console.log(formMedData);
     axios.post(`http://localhost:3000/api/read`, transactionId)
       .then(res => {
-        // console.log(res.data.txn);
-        // setNewTxnData(res.data.txn);
+        console.log(res.data.txPayload);
+        setPayloadData(res.data.txPayload);
       });
   };
+
+  console.log(payloadData);
+
   return (
     <div className='flex flex-col bg-yellow-300 h-screen w-full'>
       <NavBar />
@@ -40,6 +44,34 @@ export default function read() {
           SUBMIT
         </button>
       </form>
+      {Object.keys(payloadData).length > 0 &&
+        <div className='flex flex-col justify-center items-center bg-teal-500 mx-40 mb-10 text-xl border-black border-4 rounded-t-2xl rounded-b-2xl'>
+          <h2 className='text-black font-2xl font-bold my-5 text-center'>
+            MEDICAL RECORD
+          </h2>
+          <label className='text-lg text-black mb-10 text-center'>
+            {payloadData.name && `Name: ${payloadData.name}`}
+            <br />
+            {payloadData.age && `Age: ${payloadData.age}`}
+            <br />
+            {payloadData.dob && `Date of Birth: ${payloadData.dob}`}
+            <br />
+            {payloadData.weight && `Weight: ${payloadData.weight}`}
+            <br />
+            {payloadData.height && `Height: ${payloadData.height}`}
+            <br />
+            {payloadData.vacStat && `Vaccincation Status: ${payloadData.vacStat}`}
+            <br />
+            {payloadData.doctor && `Doctor: ${payloadData.doctor}`}
+            <br />
+            {
+              payloadData.healthCardNum &&
+              `Health Card #: ${payloadData.healthCardNum}`
+            }
+            <br />
+          </label>
+        </div>
+      }
     </div>
   );
-}
+};
