39d84a5b5c83fe4fc0ba9113b99d7cd22fd47a7b
ACF <alricfernandes11@gmail.com>
2023-09-19 14:27:27 -0400

    Using transaction ID from request body to read transaction data from the blockchain

--- src/pages/api/read.ts
+++ src/pages/api/read.ts
@@ -2,6 +2,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const { ULedgerBMSSession } = require('@uledger/uledger-sdk');
 
 export default async function readTransactionHandler(req: NextApiRequest, res: NextApiResponse) {
+  // console.log(req.body.transactionId)
+  
   try {
 
     const session = new ULedgerBMSSession({
@@ -10,8 +12,8 @@ export default async function readTransactionHandler(req: NextApiRequest, res: N
 
     const trim = false;
 
-    const transactionId = "8861c0baf78c640cff8c4c51a06cd1605fce64706e5508806546a87419e21c14";
-    // const transactionId = '5c4688d5b5d7e300c353760c7ce2c41080ba10b19848492b2c3a1ca167817c05'
+    // const transactionId = "8861c0baf78c640cff8c4c51a06cd1605fce64706e5508806546a87419e21c14";
+    const transactionId = req.body.transactionId;
 
     const bmsTxn = await session.searchTransactionById(transactionId, trim);
     const txPayload = eval('(' + bmsTxn.payload + ')');
