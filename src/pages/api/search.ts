eb56341c6735df7048a55c37c78d1c37a17bfce0
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-09-25 15:13:36 -0400

    working search func

---
+++ src/pages/api/search.ts
@@ -0,0 +1,38 @@
+const { ULedgerBMSSession } = require("@uledger/uledger-sdk");
+
+async function userHistory() {
+
+  const session = new ULedgerBMSSession({
+    url: "https://uledger.net/api/v1/bms"
+  });
+
+  // Search terms - which blockchain and which user are we looking for?
+  const blockchainId = "f78b4aeb3979871111ae2984de352dee0a3e0da01c1236274dfbfb95e2ee05e3";
+  const userIdKeyword = "healthCareProviderAddress";
+  // Pagination parameters 
+  const limit = 10;
+  const offset = 0;
+  const sort = true;
+  const trim = false;
+  const publicBool = true;
+  
+  // Send the request
+  const history = await session.userHistory(blockchainId, userIdKeyword, limit, offset, sort, trim, publicBool);
+
+  const txns = history.from.payload;
+  
+  let output: Array<object> = [];
+
+  for (let i = 0; i < txns.length; i++) {
+    let tempObject = {
+      transactionId: txns[i]["transactionId"],
+      payload: txns[i]["payload"]
+    }
+    output.push(tempObject);
+  };
+
+  console.log(output);
+
+}
+
+userHistory();
