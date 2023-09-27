b12ddf313aeb0fee54f9c27455159cfdb968284e
ACF <alricfernandes11@gmail.com>
2023-09-27 16:14:17 -0400

    Updating padding for table cells

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -69,34 +69,34 @@ export default function Search() {
           <table className='text-center table-auto'>
             <thead>
               <tr>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Transaction ID
                 </th>
-                <th className='bg-yellow-500 px-2 py-1 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Patient ID
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Name
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Age
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Date of Birth
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Weight
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Height
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Vaccination Status
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Doctor
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Health Card #
                 </th>
               </tr>
@@ -105,34 +105,34 @@ export default function Search() {
               {allSearchData.map((element) => {
                 return (
                   <tr>
-                    <td className='bg-white px-4 py-2 border-black border-2'>
+                    <td className='bg-white px-1 py-1 border-black border-2'>
                       {element.transactionId}
                     </td>
-                    <td className='bg-white px-4 py-2 border-black border-2'>
+                    <td className='bg-white px-1 py-1 border-black border-2'>
                       {element.payload.patientId}
                     </td>
-                    <td className='bg-white px-4 py-2 border-black border-2'>
+                    <td className='bg-white px-1 py-1 border-black border-2'>
                       {element.payload.name}
                     </td>
-                    <td className='bg-white px-4 py-2 border-black border-2'>
+                    <td className='bg-white px-1 py-1 border-black border-2'>
                       {element.payload.age}
                     </td>
-                    <td className='bg-white px-4 py-2 border-black border-2'>
+                    <td className='bg-white px-1 py-1 border-black border-2'>
                       {element.payload.dob}
                     </td>
-                    <td className='bg-white px-4 py-2 border-black border-2'>
+                    <td className='bg-white px-1 py-1 border-black border-2'>
                       {element.payload.weight}
                     </td>
-                    <td className='bg-white px-4 py-2 border-black border-2'>
+                    <td className='bg-white px-1 py-1 border-black border-2'>
                       {element.payload.height}
                     </td>
-                    <td className='bg-white px-4 py-2 border-black border-2'>
+                    <td className='bg-white px-1 py-1 border-black border-2'>
                       {element.payload.vacStat}
                     </td>
-                    <td className='bg-white px-4 py-2 border-black border-2'>
+                    <td className='bg-white px-1 py-1 border-black border-2'>
                       {element.payload.doctor}
                     </td>
-                    <td className='bg-white px-4 py-2 border-black border-2'>
+                    <td className='bg-white px-1 py-1 border-black border-2'>
                       {element.payload.healthCardNum}
                     </td>
                   </tr>
@@ -145,34 +145,34 @@ export default function Search() {
           <table>
             <thead>
               <tr>
-                <th className='bg-yellow-500 px-2 py-1 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Transaction ID
                 </th>
-                <th className='bg-yellow-500 px-2 py-1 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Patient ID
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Name
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Age
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Date of Birth
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Weight
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Height
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Vaccination Status
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Doctor
                 </th>
-                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                <th className='bg-yellow-500 px-1 py-1 border-black border-2'>
                   Health Card #
                 </th>
               </tr>
@@ -183,34 +183,34 @@ export default function Search() {
                   if (Object.values(element.payload).includes(searchTerm)) {
                     return (
                       <tr>
-                        <td className='bg-white px-4 py-2 border-black border-2'>
+                        <td className='bg-white px-1 py-1 border-black border-2'>
                           {element.transactionId}
                         </td>
-                        <td className='bg-white px-4 py-2 border-black border-2'>
+                        <td className='bg-white px-1 py-1 border-black border-2'>
                           {element.payload.patientId}
                         </td>
-                        <td className='bg-white px-4 py-2 border-black border-2'>
+                        <td className='bg-white px-1 py-1 border-black border-2'>
                           {element.payload.name}
                         </td>
-                        <td className='bg-white px-4 py-2 border-black border-2'>
+                        <td className='bg-white px-1 py-1 border-black border-2'>
                           {element.payload.age}
                         </td>
-                        <td className='bg-white px-4 py-2 border-black border-2'>
+                        <td className='bg-white px-1 py-1 border-black border-2'>
                           {element.payload.dob}
                         </td>
-                        <td className='bg-white px-4 py-2 border-black border-2'>
+                        <td className='bg-white px-1 py-1 border-black border-2'>
                           {element.payload.weight}
                         </td>
-                        <td className='bg-white px-4 py-2 border-black border-2'>
+                        <td className='bg-white px-1 py-1 border-black border-2'>
                           {element.payload.height}
                         </td>
-                        <td className='bg-white px-4 py-2 border-black border-2'>
+                        <td className='bg-white px-1 py-1 border-black border-2'>
                           {element.payload.vacStat}
                         </td>
-                        <td className='bg-white px-4 py-2 border-black border-2'>
+                        <td className='bg-white px-1 py-1 border-black border-2'>
                           {element.payload.doctor}
                         </td>
-                        <td className='bg-white px-4 py-2 border-black border-2'>
+                        <td className='bg-white px-1 py-1 border-black border-2'>
                           {element.payload.healthCardNum}
                         </td>
                       </tr>
