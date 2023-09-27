8d3db54261d62aabb211163a1011d368abf9ed47
ACF <alricfernandes11@gmail.com>
2023-09-27 14:47:10 -0400

    Adding search functionality

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -3,27 +3,11 @@ import NavBar from '../components/NavBar';
 import Input from '../components/Input';
 import axios from 'axios';
 
-// let obj = [
-//   {
-//     transactionId: 'txId1',
-//     payload: { patientId: '1', name: 'abc', age: '123' }
-//   },
-//   {
-//     transactionId: 'txId2',
-//     payload: { patientId: '2', name: 'def', age: '456' }
-//   },
-//   {
-//     transactionId: 'txId3',
-//     payload: { patientId: '3', name: 'ghi', age: '789' }
-//   },
-// ];
-
 export default function Search() {
   const [searchTerm, setSearchTerm] = useState('');
   const [all, setAll] = useState(true);
   const [searchButton, setSearchButton] = useState(false);
   const [allSearchData, setAllSearchData] = useState([]);
-
 
   useEffect(() => {
     axios.get(`http://localhost:3000/api/search`)
@@ -33,15 +17,9 @@ export default function Search() {
       });
   }, []);
 
-
   const onChange = (e) => {
     setSearchTerm(e.target.value);
     if (searchButton) setSearchButton(false);
-    // if (e.target.value !== '') {
-    //   setAll(false);
-    // } else {
-    //   setAll(true);
-    // }
   };
 
   const onClick = (e) => {
@@ -51,17 +29,18 @@ export default function Search() {
       if (all) setAll(false);
       if (!searchButton) setSearchButton(true);
     }
-    if(e.target.name === 'allData') {
-      if(all === false) setAll(true)
+    if (e.target.name === 'allData') {
+      if (!all) setAll(true);
       setAll(true);
       setSearchButton(false);
+      setSearchTerm('');
     }
   };
 
   return (
     <div className='flex flex-col items-center bg-yellow-500 text-black h-screen px-10'>
       <NavBar />
-      <div className='flex flex-col items-center my-10 py-10 bg-teal-500 gap-5 px-5 text-sm border-black border-4 rounded-t-2xl rounded-b-2xl max-w-full overflow-x-auto'>
+      <div className='flex flex-col items-center my-10 py-10 bg-teal-500 gap-5 px-5 text-sm border-black border-4 rounded-t-2xl rounded-b-2xl max-w-full overflow-x-auto w-full h-full'>
         <form className='flex justify-center items-center gap-5 w-full'>
           <Input
             className='text-center border-black border-2 text-black w-full'
@@ -90,7 +69,7 @@ export default function Search() {
           <table className='text-center table-auto'>
             <thead>
               <tr>
-                <th className='bg-yellow-500 px-2 py-1 border-black border-2 w-1/6'>
+                <th className='bg-yellow-500 px-2 py-1 border-black border-2'>
                   Transaction ID
                 </th>
                 <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
@@ -156,13 +135,37 @@ export default function Search() {
             </tbody>
           </table>
         }
-        {(!all && searchTerm && searchButton && (allSearchData.length > 0)) &&
+        {(!all && searchButton && (allSearchData.length > 0)) &&
           <table>
             <thead>
               <tr>
-                <th>Transaction ID</th>
-                <th>Name</th>
-                <th>Age</th>
+                <th className='bg-yellow-500 px-2 py-1 border-black border-2'>
+                  Transaction ID
+                </th>
+                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                  Name
+                </th>
+                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                  Age
+                </th>
+                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                  Date of Birth
+                </th>
+                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                  Weight
+                </th>
+                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                  Height
+                </th>
+                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                  Vaccination Status
+                </th>
+                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                  Doctor
+                </th>
+                <th className='bg-yellow-500 px-4 py-2 border-black border-2'>
+                  Health Card #
+                </th>
               </tr>
             </thead>
             <tbody>
@@ -171,9 +174,33 @@ export default function Search() {
                   if (Object.values(element.payload).includes(searchTerm)) {
                     return (
                       <tr>
-                        <td>{element.payload.patientId}</td>
-                        <td>{element.payload.name}</td>
-                        <td>{element.payload.age}</td>
+                        <td className='bg-white px-4 py-2 border-black border-2 text-sm whitespace-normal overflow-wrap-break-word w-1/6'>
+                          {element.transactionId}
+                        </td>
+                        <td className='bg-white px-4 py-2 border-black border-2 whitespace-normal break-words'>
+                          {element.payload.name}
+                        </td>
+                        <td className='bg-white px-4 py-2 border-black border-2'>
+                          {element.payload.age}
+                        </td>
+                        <td className='bg-white px-4 py-2 border-black border-2'>
+                          {element.payload.dob}
+                        </td>
+                        <td className='bg-white px-4 py-2 border-black border-2'>
+                          {element.payload.weight}
+                        </td>
+                        <td className='bg-white px-4 py-2 border-black border-2'>
+                          {element.payload.height}
+                        </td>
+                        <td className='bg-white px-4 py-2 border-black border-2'>
+                          {element.payload.vacStat}
+                        </td>
+                        <td className='bg-white px-4 py-2 border-black border-2'>
+                          {element.payload.doctor}
+                        </td>
+                        <td className='bg-white px-4 py-2 border-black border-2'>
+                          {element.payload.healthCardNum}
+                        </td>
                       </tr>
                     );
                   }
@@ -181,31 +208,6 @@ export default function Search() {
             </tbody>
           </table>
         }
-        {/* {(!all && searchTerm && !searchButton) &&
-          <table>
-            <thead>
-              <tr>
-                <th>Transaction ID</th>
-                <th>Name</th>
-                <th>Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                obj.map((element) => {
-                  if (Object.values(element.payload).includes(searchTerm)) {
-                    return (
-                      <tr>
-                        <td>{element.payload.patientId}</td>
-                        <td>{element.payload.name}</td>
-                        <td>{element.payload.age}</td>
-                      </tr>
-                    );
-                  }
-                })}
-            </tbody>
-          </table>
-        } */}
       </div>
     </div>
   );
